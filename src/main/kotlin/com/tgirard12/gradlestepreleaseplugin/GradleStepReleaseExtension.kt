d474b425f5468cf03efeaaf432410b0fbfb83854
Thomas Girard <thomas.girard12@gmail.com>
2018-02-16 21:36:56 +0100

    check Properties Step from .properties files

--- src/main/kotlin/com/tgirard12/gradlestepreleaseplugin/GradleStepReleaseExtension.kt
+++ src/main/kotlin/com/tgirard12/gradlestepreleaseplugin/GradleStepReleaseExtension.kt
@@ -1,11 +1,18 @@
 package com.tgirard12.gradlestepreleaseplugin
 
+import java.io.File
+import java.util.*
+
 
 open class GradleStepReleaseExtension {
+
+    // Const
+    val gradleProperties = "gradle.properties"
 
     var stepResult = mutableMapOf<Int, Any?>()
     var steps = mutableListOf<Step>()
 
+    // Simples
 
     fun question(message: String) = Step(
             title = "Question",
@@ -18,5 +25,22 @@ open class GradleStepReleaseExtension {
             step = step
     )
 
+    // Properties
 
+    fun checkProperties(propsKeys: List<String>, propFile: String = gradleProperties) = Step(
+            title = "Check properties",
+            validation = Step.Validation("All is OK") {
+                Properties().apply {
+                    File(propFile)
+                            .inputStream()
+                            .use { load(it) }
+                }
+                        .filterKeys { it -> propsKeys.contains(it) }
+                        .map { k -> "${k.key}=${k.value}" }
+                        .sorted()
+                        .joinToString("\n", postfix = "\n")
+                        .console()
+            },
+            step = {}
+    )
 }
