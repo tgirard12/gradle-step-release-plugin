8f0893b2ad4ee30a7ed11fa0dd2da047b4e7d116
Thomas Girard <thomas.girard12@gmail.com>
2018-02-16 21:36:43 +0100

    simple question and lambda step

--- src/main/kotlin/com/tgirard12/gradlestepreleaseplugin/GradleStepReleaseExtension.kt
+++ src/main/kotlin/com/tgirard12/gradlestepreleaseplugin/GradleStepReleaseExtension.kt
@@ -6,4 +6,17 @@ open class GradleStepReleaseExtension {
     var stepResult = mutableMapOf<Int, Any?>()
     var steps = mutableListOf<Step>()
 
+
+    fun question(message: String) = Step(
+            title = "Question",
+            validation = Step.Validation(message),
+            step = { null }
+    )
+
+    fun step(title: String = "step", step: () -> Any?) = Step(
+            title = title,
+            step = step
+    )
+
+
 }
