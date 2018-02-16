0a9d168ecd5602578022d6f717fcc3798050d6b7
Thomas Girard <thomas.girard12@gmail.com>
2018-02-16 21:36:30 +0100

    Gradle Task to iterate over all steps

--- src/main/kotlin/com/tgirard12/gradlestepreleaseplugin/GradleStepReleaseTask.kt
+++ src/main/kotlin/com/tgirard12/gradlestepreleaseplugin/GradleStepReleaseTask.kt
@@ -2,6 +2,8 @@ package com.tgirard12.gradlestepreleaseplugin
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 
 open class GradleStepReleaseTask : DefaultTask() {
@@ -12,6 +14,50 @@ open class GradleStepReleaseTask : DefaultTask() {
 
     @TaskAction
     fun gradleStepReleaseTask() {
-        
+        if (extension.steps.isEmpty())
+            throw IllegalArgumentException(
+                    """You must define at least one Step in releaseStep {
+                        |   steps = [...]
+                        |}""".trimMargin())
+
+        extension.steps.forEachIndexed { index, step ->
+            "## Step : ${step.title}".console()
+            "".console()
+            step.validation?.let {
+                step.validation.beforeMessage.invoke()
+                "${step.validation.message} [y, yes] ?".validateQuestion()
+            }
+            step.step()?.let { extension.stepResult.put(index, it) }
+            "".console()
+        }
     }
+
+    //
+    fun read() = readLine().apply { this?.console() }
+
+    fun String.console(): Unit = println(this)
+
+    fun String.validateQuestion() {
+        this.console()
+        val line = read()
+        if (line != "y" && line != "yes")
+            throw IllegalArgumentException("Validation fail, Exit task ")
+    }
+
+    fun String.runCommand() {
+        try {
+            "$> $this".console()
+            val parts = this.split("\\s".toRegex())
+            val proc = ProcessBuilder(*parts.toTypedArray())
+                    .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                    .redirectError(ProcessBuilder.Redirect.PIPE)
+                    .start()
+
+            proc.waitFor(10, TimeUnit.SECONDS)
+            proc.inputStream.bufferedReader().readText().console()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
 }
